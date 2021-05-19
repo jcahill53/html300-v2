ec89039d3feb8444ae8e65447ca8797a1e198e02
JCahill <cahilljm@aol.com>
2021-05-19 02:30:38 -0700

    Added validation on form

---
+++ lesson06/assignment/form2.js
@@ -0,0 +1,18 @@
+function validateForm() {
+    var nameValue = document.forms["factForm"]["name"].value;
+    var eMailValue = document.forms["factForm"]["email"].value;
+    var textArea1Value = document.forms["factForm"]["textArea1"].value;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (nameValue == "") {
+        alert("Name must be filled out");
+        return false;
+    } else if (eMailValue == "") {
+        alert("Email must be provided");
+        return false;
+    } else if (textArea1Value == "") {
+        alert("Add a fun fact in the text area");
+        return false;
+    } else alert("Thanks for submitting your fun fact!");
+
+}
